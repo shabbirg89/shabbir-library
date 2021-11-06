e01a2c6561169c7bfa9f13507bd7daefdc7e3f64
Shabbir Governor <45769325+shabbirg89@users.noreply.github.com>
2021-11-06 16:58:22 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='shabbir_library',
+      version='0.11',
+      description='operation & greeting library',
+      packages=['shabbir_library'],
+      author_email='shabbirg89@gmail.com',
+      zip_safe=False)
