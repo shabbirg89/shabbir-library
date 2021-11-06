0b3c9ec0a33cca27a3fb87b4915edad4aa395aca
Shabbir Governor <45769325+shabbirg89@users.noreply.github.com>
2021-11-06 16:59:08 +0530

    Add files via upload

---
+++ shabbir-library/__init__.py
@@ -0,0 +1,6 @@
+from .operation import add
+from .operation import subtract
+from .operation import multiply
+from .operation import divide
+from .operation import greet
+from .operation import square
---
+++ shabbir-library/operation.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def greet(name):
+    return "Hello, " + name
+
+
+def square(a):
+    return a * a
